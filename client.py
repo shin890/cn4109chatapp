717ab099480dff8d23c248007cbe7ab5251d44c4
Shuva <38221577+shin890@users.noreply.github.com>
2024-06-28 22:47:28 +0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,54 +1,127 @@
 import socket
-import random
-from threading import Thread
-from datetime import datetime
-from colorama import Fore, init, Back
-
-#init colors
-init()
-
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-]
-
-client_color = random.choice(colors)
-
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 9001
-separator_token = "<SEP>" 
-
-s = socket.socket()
-print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
-s.connect((SERVER_HOST, SERVER_PORT))
-print("[+] Connected.")
+import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
 
 
-name = input("Enter your name: ")
+HOST='127.0.0.1'
+PORT=4109
 
-def listen_for_messages():
-    while True:
-        message = s.recv(1024).decode()
-        print("\n" + message)
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Chat Application Project")
+root.resizable(False, False)
 
-# make a thread that listens for messages to this client & print them
-t = Thread(target=listen_for_messages)
-# make the thread daemon so it ends whenever the main thread ends
-t.daemon = True
-# start the thread
-t.start()
+DARK_GREY='#121212'
+MEDIUM_GREY='#1F1B24'
+OCEAN_BLUE='#464EB8'
+FONT=("Helvetica",17)
+BUTTON_FONT=("Oswald",15)
+SMALL_FONT=("Helvetica",13)
+WHITE='white'
 
-while True:
-    # input message we want to send to the server
-    to_send =  input()
-    # a way to exit the program
-    if to_send.lower() == 'q':
-        break
-    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    s.send(to_send.encode())
+def add_message(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + '\n')
+    message_box.config(state=tk.DISABLED)
+
+def connect():
+    #connect to the server
+    try:
+        client.connect((HOST,PORT))
+        add_message("[SERVER] Successfully Connected to the Server")
+    except:
+        
+        messagebox.showerror("Error",f"Unable to connect to the server {HOST}:{PORT}")
+        exit(0)
+
+    
+    username=username_textbox.get()
+
+    if username !='':
+        client.sendall(username.encode())
+        username_textbox.config(state=tk.DISABLED)
+        username_button.config(state=tk.DISABLED)
+        
+    else:
+        messagebox.showerror("Invalid Username","Username cannot be empty")
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+    
 
 
-s.close()
+def send_message():
+    message = message_textbox.get()
+
+    if message != '':
+        client.sendall(message.encode())
+        message_textbox.delete(0,len(message))
+    else:
+        messagebox.showerror("Empty message","Messsage cannot be empty")
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=4)
+root.grid_rowconfigure(2, weight=1)
+
+top_frame= tk.Frame(root, width=600, height =100, bg=DARK_GREY)
+top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+middle_frame= tk.Frame(root, width=600, height =400, bg=MEDIUM_GREY)
+middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+
+bottom_frame= tk.Frame(root, width=600, height =100, bg=DARK_GREY)
+bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+
+username_label = tk.Label(top_frame, text="Enter username: ", font=FONT, bg=DARK_GREY, fg=WHITE)
+username_label.pack(side=tk.LEFT, padx=10)
+
+username_textbox= tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=20)
+username_textbox.pack(side=tk.LEFT)
+username_button= tk.Button(top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, width=8, fg=WHITE, command=connect)
+username_button.pack(side=tk.RIGHT, padx=25)
+
+
+message_textbox= tk.Entry(bottom_frame, font=FONT, bg='#007fff', fg=WHITE, width=38)
+message_textbox.pack(side=tk.LEFT, padx=10)
+
+message_button= tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, width=8, command=send_message)
+message_button.pack(side=tk.LEFT, padx=5)
+
+message_box= scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg= WHITE, width =67, height=26.5)
+message_box.config(state=tk.DISABLED)
+message_box.pack(side=tk.LEFT)
+
+
+#for talking to server
+
+#Server jemon clien theke message shuntese, client er o server theke message shunte hobe
+def listen_for_messages_from_server(client):
+
+    while 1:
+
+        message=client.recv(2048).decode('utf8')
+
+        if message != '':
+            username=message.split("~")[0]
+            content=message.split("~")[1 ]
+
+            add_message(f"[{username}] {content}")
+        else:
+            messagebox.showerror("Error","Message received from client is empty")
+
+
+        
+
+
+
+def main():
+
+    #to render the tkinter window
+    root.mainloop()
+    
+
+if __name__=='__main__':
+    main()
--- server.py
+++ server.py
@@ -1,59 +1,81 @@
 import socket
-from threading import Thread
+import threading
 
-SERVER_HOST='0.0.0.0'
-SERVER_PORT=9001
-seperator_token="<SEP>"
-#sep is used for separating client name and message
+HOST='127.0.0.1'
+PORT=4109
+LISTENER_LIMIT=5
+active_clients= [] #list of all currently connected user
 
-#initialize list/set of all connected client's socket
-client_sockets=set()
+#Function for listen upcoming messages from a client
+def listen_for_messages(client, username):
 
-#for creating a tcp socket
-s=socket.socket()
+    while 1:
 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-#making the port as reusable port
+        message=client.recv(2048).decode('utf-8')
 
-s.bind((SERVER_HOST, SERVER_PORT)) #For binding
+        if message!= '':
+            final_msg=username+ '~'+ message
+            send_messages_to_all(message=final_msg)
+        else:
+            print(f"The message sent from client {username} is empty")
 
-s.listen(5) #listen for upcoming connections
+#Function to send message to a single client
+def send_message_to_client(client,message):
+    
+    client.sendall(message.encode())
+def send_messages_to_all(message):
 
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+    for user in active_clients:
+
+        send_message_to_client(user[1], message)
+
+def client_handler(client):
+    while True:
+        username=client.recv(2048).decode('utf-8')
+
+        if username!= '' :
+            active_clients.append((username,client))
+            prompt_message = "Server~" + f"{username} added to the chat"
+            send_messages_to_all(prompt_message)
+            break
+        else:
+            print("Client username is empty")
+        
+    threading.Thread(target=listen_for_messages, args=(client, username,)).start()
+
+    
 
 
-def listen_for_client(cs):
-    #This function keeps listening for a message from cs socket
-    #Whenever a message is received, broadcast it to all other connected clients
+def main():
+    #Creating the socket class object(AF_INET=ipv4 use kortesti, sock_stream boltese tcp use kortesi, )
+    #sock_dgram for udp 
+    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server.bind((HOST, PORT))
+        print(f"Running the server on {HOST}:{PORT}")
+    except:
+        print(f"Unable to bind to host  {HOST} and {PORT}")
+        #bind korte parbe na jokhon host ar port bind korte parbe na
+
+    #SET SERVER LIMIT
+    server.listen(LISTENER_LIMIT)
+
+    #this while will keep listening to client connections
 
     while True:
+        client,address=server.accept()
+        print(f"Successfully connected to client :{address[0]}:{address[1]}")
 
-        try:
-            msg=cs.recv(1024).decode()
-        except Exception as e:
-            print(f"[!]Error: {e}")
-            client_sockets.remove(cs)
-        else:
-            msg=msg.replace(seperator_token, ": ")
-        
-        for client_socket in client_sockets:
-            client_socket.send(msg.encode())
 
-while True:
-    # for listening for new connections all the time
-    client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
-    # add the new connected client to connected sockets
-    client_sockets.add(client_socket)
-    # start a new thread that listens for each client's messages
-    t = Thread(target=listen_for_client, args=(client_socket, ))
-    # make the thread daemon so it ends whenever the main thread ends
-    t.daemon = True
-    # start the thread
-    t.start()
+        #we want our function to run concurrently with the server side function
 
-# close client sockets
-for cs in client_sockets:
-    cs.close()
-# close server socket
-s.close()
+        threading.Thread(target=client_handler, args=(client, )).start()
+
+if __name__=='__main__':
+    main()
+#main function will be called if the server.py
+#was called directly, else if called from a module
+#it won't be called
+
+
